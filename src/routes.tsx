e708f97d6e1859a035143ba8bfc1fd2d2f6d6b16
Halk-lai Liff <halkliff@pm.me>
2019-08-12 13:00:16 -0300

    Routing instalation

---
+++ src/routes.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Route, BrowserRouter, RouteProps } from "react-router-dom";
+
+/* const Routes: React.FunctionComponent = (
+  props: React.PropsWithChildren<any>
+) => {
+  return (
+    <BrowserRouter>
+      <Route path="/" />
+    </BrowserRouter>
+  );
+}; */
+
+export const routes: RouteProps[] = [
+  {
+    p
+  }
+];
