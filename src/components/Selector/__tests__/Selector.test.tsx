1e7a3f64617e04ac294324b3f85c58a484b80b9d
Halk-lai Liff <halkliff@pm.me>
2019-08-15 09:51:16 -0300

    Selector component render tests

---
+++ src/components/Selector/__tests__/Selector.test.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {create, ReactTestRenderer} from 'react-test-renderer';
+
+// This component is dependent of the redux store, therefore we must have the store
+// provider available for the component.
+
+import { Provider } from 'react-redux';
+import store from 'store';
+
+import Selector from '../Selector';
+
+describe('Selector Component tests', () => {
+
+  it('Renders the component without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(
+      <Provider store={store}>
+        <Selector />
+      </Provider>, 
+      div
+    );
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('Renders the component correctly', () => {
+    const selector: ReactTestRenderer = create(
+      <Provider store={store}>
+        <Selector />
+      </Provider>,
+    );
+    expect(selector.toJSON()).toMatchSnapshot();
+  });
+});
