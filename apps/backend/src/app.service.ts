6d39ce64bfcc635ecbd5f207176c4f961929d89a
Halk-lai Liff <halkliff@pm.me>
2019-09-10 14:21:30 -0300

    Upgrade eslint on applications and scaffold client on client module in backend

--- apps/backend/src/app.service.ts
+++ apps/backend/src/app.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+export default class AppService {
+  protected message = 'Hello World!';
+
+  public getHello(): string {
+    return this.message;
   }
 }
---
+++ apps/backend/src/client/client.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { ClientService } from './services/client/client.service';
+import ClientController from './client.controller';
+
+@Module({
+  providers: [ClientService],
+  controllers: [ClientController],
+})
+export default class ClientModule {}
