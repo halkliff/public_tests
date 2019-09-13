0851d76a280e23264443baf149a9d5c8dd977246
Halk-lai Liff <halkliff@pm.me>
2019-09-13 12:19:46 -0300

    Added `NetworkResponse` to use across apps

--- libs/types/dist/Client/Client.interface.d.ts
+++ libs/types/dist/Client/Client.interface.d.ts
@@ -1,12 +1,11 @@
 import Contacts from './Contacts.interface';
-
 declare type ClientType = 'fisico' | 'juridico';
 export default interface Client {
-  id: string;
-  name: string;
-  clientType: ClientType;
-  document: string;
-  parsedDocument?: string;
-  contacts: Contacts;
+    id: string;
+    name: string;
+    clientType: ClientType;
+    document: string;
+    parsedDocument?: string;
+    contacts: Contacts;
 }
 export {};
--- libs/types/dist/Client/index.d.ts
+++ libs/types/dist/Client/index.d.ts
@@ -1,2 +1,2 @@
-export * from './Contacts.interface';
+export { default as Contacts } from './Contacts.interface';
 export { default as Client } from './Client.interface';
