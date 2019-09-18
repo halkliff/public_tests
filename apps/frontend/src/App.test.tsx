eb397f48eab0ca1cb0aae3a4e5750c20a426384f
Halk-lai Liff <halkliff@pm.me>
2019-09-18 10:30:13 -0300

    Update main test for enzyme testing

--- apps/frontend/src/App.test.tsx
+++ apps/frontend/src/App.test.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import { Provider } from 'react-redux';
 import App from './App';
+import store from './store';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('Application takeoff tests', () => {
+  it('Should render the application without crashing', () => {
+    const wrapper = shallow(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+    expect(wrapper).toBeDefined();
+  });
 });
